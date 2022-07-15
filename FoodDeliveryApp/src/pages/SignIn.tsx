f37bb0220caf26d3814f12b4f1f5dea51b6f0b87
soyeon100 <dev.dorahee100@gmail.com>
2022-07-15 17:18:09 +0900

    Login 화면 만들기
    StyleSheet 적용하기
    
    Pressable태그 속성 중 disabled에 조건 주기 (email && password값이 있는 경우 true, 없는 경우 false 처리)
    
    onChangeText 함수 사용 시 TextInput 입력하는 값이 text 매개변수로 전달된다.
    각각의 함수에 text 매개변수로 email, password를 useState함수를 사용하여 저장한다.
    
    TODO
    매개변수 text 값에 Type 지정해주기
    함수에 return 되는 값이 없어서 : void 도 넣어줌
    
    settings -> prettier 설정 체크함

--- FoodDeliveryApp/src/pages/SignIn.tsx
+++ FoodDeliveryApp/src/pages/SignIn.tsx
@@ -1,11 +1,96 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useCallback, useState} from 'react';
+import {
+  StyleSheet,
+  Alert,
+  Pressable,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
 
 function SingIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onChangeEmail = useCallback((text: string): void => {
+    setEmail(text);
+  }, []);
+  const onChangePassword = useCallback((text: string): void => {
+    setPassword(text);
+  }, []);
+  const onSubmit = useCallback(() => {
+    Alert.alert('알림', '안녕~');
+  }, []);
+
+  const canGoNext = email && password;
+
   return (
     <View>
-      <Text>로그인</Text>
+      <View style={styles.inputWrapper}>
+        <Text style={styles.labelText}>이메일</Text>
+        <TextInput
+          style={styles.labelTextInput}
+          placeholder="이메일을 입력해주세요."
+          onChangeText={onChangeEmail}
+        />
+      </View>
+      <View style={styles.inputWrapper}>
+        <Text style={styles.labelText}>비밀번호</Text>
+        <TextInput
+          style={styles.labelTextInput}
+          placeholder="비밀번호를 입력해주세요."
+          onChangeText={onChangePassword}
+        />
+      </View>
+      <View style={styles.buttonZone}>
+        <Pressable
+          onPress={onSubmit}
+          style={
+            !canGoNext
+              ? styles.loginButton
+              : StyleSheet.compose(styles.loginButton, styles.loginButtonActive)
+          }
+          disabled={!canGoNext}>
+          <Text style={styles.loginButtonText}>로그인</Text>
+        </Pressable>
+        <Pressable onPress={onSubmit} style={styles.loginButton}>
+          <Text style={styles.loginButtonText}>회원가입하기</Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  inputWrapper: {
+    padding: 20,
+  },
+  labelText: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    marginBottom: 10,
+  },
+  labelTextInput: {
+    padding: 5,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+  loginButton: {
+    backgroundColor: 'gray',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 10,
+    marginBottom: 10,
+  },
+  loginButtonActive: {
+    backgroundColor: 'blue',
+  },
+  loginButtonText: {
+    color: '#fff',
+    fontSize: 16,
+  },
+  buttonZone: {
+    alignItems: 'center',
+  },
+});
+
 export default SingIn;
