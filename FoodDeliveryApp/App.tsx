4995caf7924ec059dc8a5df55d08603ea2e6427f
soyeon100 <dev.dorahee100@gmail.com>
2022-07-17 16:55:22 +0900

    Provider 바깥에서는 useSelector 사용 불가
    AppInner 컴포넌트 생성해서 분리해준다.
    AppInner 안에 useSelector 사용해주고 Provider 안에 컴포넌트를 import 해서 사용해준다.

--- FoodDeliveryApp/App.tsx
+++ FoodDeliveryApp/App.tsx
@@ -1,15 +1,7 @@
 import * as React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import Settings from './src/pages/Settings';
-import Orders from './src/pages/Orders';
-import Delivery from './src/pages/Delivery';
-import SignIn from './src/pages/SignIn';
-import SignUp from './src/pages/SignUp';
-import {Provider, useSelector} from 'react-redux';
+import {Provider} from 'react-redux';
 import store from './src/store';
-import {RootState} from './src/store/reducer';
+import AppInner from './AppInner';
 
 export type LoggedInParamList = {
   Orders: undefined;
@@ -23,49 +15,11 @@ export type RootStackParamList = {
   SignUp: undefined;
 };
 
-const Tab = createBottomTabNavigator();
-const Stack = createNativeStackNavigator();
-
 function App() {
-  // 리덕스에서 관리하는 상태 값으로 email 존재 여부로 로그인 여부 확인
-  const isLoggedIn = useSelector((state: RootState) => !!state.user.email);
   // const [isLoggedIn, setLoggedIn] = useState(false);
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        {isLoggedIn ? (
-          <Tab.Navigator>
-            <Tab.Screen
-              name="Orders"
-              component={Orders}
-              options={{title: '오더 목록'}}
-            />
-            <Tab.Screen
-              name="Delivery"
-              component={Delivery}
-              options={{headerShown: false}}
-            />
-            <Tab.Screen
-              name="Settings"
-              component={Settings}
-              options={{title: '내 정보'}}
-            />
-          </Tab.Navigator>
-        ) : (
-          <Stack.Navigator>
-            <Stack.Screen
-              name="SignIn"
-              component={SignIn}
-              options={{title: '로그인'}}
-            />
-            <Stack.Screen
-              name="SignUp"
-              component={SignUp}
-              options={{title: '회원가입'}}
-            />
-          </Stack.Navigator>
-        )}
-      </NavigationContainer>
+      <AppInner />
     </Provider>
   );
 }
---
+++ FoodDeliveryApp/AppInner.tsx
@@ -0,0 +1,56 @@
+import Orders from './src/pages/Orders';
+import Delivery from './src/pages/Delivery';
+import Settings from './src/pages/Settings';
+import SignIn from './src/pages/SignIn';
+import SignUp from './src/pages/SignUp';
+import {NavigationContainer} from '@react-navigation/native';
+import * as React from 'react';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {useSelector} from 'react-redux';
+import {RootState} from './src/store/reducer';
+
+const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
+function AppInner() {
+  // 리덕스에서 관리하는 상태 값으로 email 존재 여부로 로그인 여부 확인
+  const isLoggedIn = useSelector((state: RootState) => !!state.user.email);
+  return (
+    <NavigationContainer>
+      {isLoggedIn ? (
+        <Tab.Navigator>
+          <Tab.Screen
+            name="Orders"
+            component={Orders}
+            options={{title: '오더 목록'}}
+          />
+          <Tab.Screen
+            name="Delivery"
+            component={Delivery}
+            options={{headerShown: false}}
+          />
+          <Tab.Screen
+            name="Settings"
+            component={Settings}
+            options={{title: '내 정보'}}
+          />
+        </Tab.Navigator>
+      ) : (
+        <Stack.Navigator>
+          <Stack.Screen
+            name="SignIn"
+            component={SignIn}
+            options={{title: '로그인'}}
+          />
+          <Stack.Screen
+            name="SignUp"
+            component={SignUp}
+            options={{title: '회원가입'}}
+          />
+        </Stack.Navigator>
+      )}
+    </NavigationContainer>
+  );
+}
+export default AppInner;
